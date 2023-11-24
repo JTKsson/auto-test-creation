35290604e5da0139893129d60dda6abbbbd77258
Timothy Karlsson <timothy.karlsson@edu.cmeducations.se>
2023-11-24 09:20:26 +0100

    forgot to save a file from conflict

--- src/Components/Pokemons/index.jsx
+++ src/Components/Pokemons/index.jsx
@@ -1,10 +1,5 @@
 import React, { useState } from "react";
 import CalculateWinner from "../CalculateWinner";
-<<<<<<< HEAD
-
-const PokemonVote = ({ name }) => {
-  console.log("pokemonvote name:", name)
-=======
 import PokemonInput from "../PokemonInputField";
 
 const PokemonVote = () => {
@@ -12,7 +7,6 @@ const PokemonVote = () => {
   const [charmanderName, setCharmanderName] = useState('Charmander');
   const [squirtleName, setSquirtleName] = useState('Squirtle');
 
->>>>>>> feature/pokemonName-input-field
   const [bulbasaurVote, setBulbasaurVote] = useState(0);
   const [charmanderVote, setCharmanderVote] = useState(0);
   const [squirtleVote, setSquirtleVote] = useState(0);
@@ -29,12 +23,6 @@ const PokemonVote = () => {
     setSquirtleVote(squirtleVote + 1);
   };
 
-<<<<<<< HEAD
-
-  return (
-    <div>
-      {name && <p data-testid="user-input-display">Welcome {name}!</p>}
-=======
   const handleBulbasaurNameChange = (name) => {
     setBulbasaurName(name);
   };
@@ -49,7 +37,6 @@ const PokemonVote = () => {
 
   return (
     <div>
->>>>>>> feature/pokemonName-input-field
       <CalculateWinner
         bulbasaurVote={bulbasaurVote}
         charmanderVote={charmanderVote}
@@ -57,26 +44,6 @@ const PokemonVote = () => {
       />
       <div>
         <div>
-<<<<<<< HEAD
-          <h3>Bulbasaur</h3>
-          {/* Add your Bulbasaur UI elements here */}
-          <button onClick={voteBulbasaur}>Vote Bulbasaur</button>
-          <p>{bulbasaurVote} votes</p>
-        </div>
-
-        <div>
-          <h3>Charmander</h3>
-          {/* Add your Charmander UI elements here */}
-          <button onClick={voteCharmander}>Vote Charmander</button>
-          <p>{charmanderVote} votes</p>
-        </div>
-
-        <div>
-          <h3>Squirtle</h3>
-          {/* Add your Squirtle UI elements here */}
-          <button onClick={voteSquirtle}>Vote Squirtle</button>
-          <p>{squirtleVote} votes</p>
-=======
           <h3>{bulbasaurName}</h3>
           <PokemonInput data-testid="bulbasaur-input" onNameChange={handleBulbasaurNameChange} />
           <img src="" alt=""/>
@@ -98,7 +65,6 @@ const PokemonVote = () => {
           <img src="" alt=""/>
           <button onClick={voteSquirtle}>Vote {squirtleName}</button>
           <p data-testid='paragraph'>{squirtleVote} votes</p>
->>>>>>> feature/pokemonName-input-field
         </div>
       </div>
     </div>
